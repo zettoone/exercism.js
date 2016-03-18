82479c9c2332220f38d4f6689bc0ce4fe507f1bb
Stephen Li <stephen.li@D2L.com>
2016-03-18 11:19:07 -0400

    refactor hamming

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,12 +1,11 @@
 'use strict';
-let hamming = function(s, t) {
+function hamming(s, t) {
   if (s.length !== t.length) {
     throw new Error('DNA strands must be of equal length.');
   }
 
-  const n = s.length;
-  let count = 0;
-  for (let i = 0; i < n; ++i) {
+  var count = 0;
+  for (let i = 0; i < s.length; ++i) {
     if (s[i] !== t[i]) {
       ++count;
     }
